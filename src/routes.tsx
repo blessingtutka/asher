520fe1dab02fdde40b2473bbc8496570bd1896d6
blessingtutka <blessingtutka298@gmail.com>
2024-06-18 19:20:43 +0200

    chore: migrate to typescript

---
+++ src/routes.tsx
@@ -0,0 +1,13 @@
+import Layout from './components/layout/Layout';
+import HomePage from './pages/HomePage';
+import { Navigate } from 'react-router-dom';
+const routes = [
+    {
+        path: '/',
+        element: <Layout />,
+        children: [{ path: '', element: <HomePage />, exact: true }],
+    },
+    { path: '*', element: <Navigate to='/' /> }, // Redirect unknown paths to home
+];
+
+export default routes;

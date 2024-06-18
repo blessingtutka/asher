6bb854cb80dec53c2c81c0f391de0fac34d0fda3
blessingtutka <blessingtutka298@gmail.com>
2024-06-18 22:33:45 +0200

    chore: configure vite for Render deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const port = process.env.PORT ? Number(process.env.PORT) : 5173;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        port: port,
+        host: '0.0.0.0',
+    },
+});

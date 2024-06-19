3a56adf1a381a6cd320ead186a4481df99c16370
blessingtutka <blessingtutka298@gmail.com>
2024-06-19 08:17:50 +0200

    chore: setup tailwind in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
 import react from '@vitejs/plugin-react';
 
 const port = process.env.PORT ? Number(process.env.PORT) : 5173;
@@ -6,6 +7,11 @@ const port = process.env.PORT ? Number(process.env.PORT) : 5173;
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    css: {
+        postcss: {
+            plugins: [tailwindcss()],
+        },
+    },
     server: {
         port: port,
         host: '0.0.0.0',
